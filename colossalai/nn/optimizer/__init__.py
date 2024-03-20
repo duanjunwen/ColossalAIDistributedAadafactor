43ddfaf350559d4f7557eee8afe819cbe5c1b9d8
duanjunwen <935724073@qq.com>
2024-03-20 13:57:16 +0800

    [feature] add DistributedAdafactor;

---
+++ colossalai/nn/optimizer/__init__.py
@@ -0,0 +1,11 @@
+from .cpu_adam import CPUAdam
+from .fused_adam import FusedAdam
+from .fused_lamb import FusedLAMB
+from .fused_sgd import FusedSGD
+from .hybrid_adam import HybridAdam
+from .lamb import Lamb
+from .lars import Lars
+from .adafactor import Adafactor
+from .distributed_adafactor import DistributedAdaFactor
+
+__all__ = ["FusedLAMB", "FusedAdam", "FusedSGD", "Lamb", "Lars", "CPUAdam", "HybridAdam", "Adafactor", "DistributedAdaFactor"]
